df92b7fec300d8d076feed2587d9699bcea0b118
Администратор <12345kolt@gmail.com>
2021-03-12 18:58:09 +0300

    v-for, деректива вывода шаблонов

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <section id="app">
     <ul>
-     <!-- <li v-for="(color, index) in colors" :key="index">Это индекс: {{ index }} это цвет: {{ color }}</li>-->
-     <!-- <li v-for="user in users" :key="user.id">{{ user.name }} {{ user.age }}</li>-->
-      <li v-for="(value, name, index) in product" :key="value">{{index}} {{name}} {{value}}</li>
+      <!-- <li v-for="(color, index) in colors" :key="index">Это индекс: {{ index }} это цвет: {{ color }}</li>-->
+      <!-- <li v-for="user in users" :key="user.id">{{ user.name }} {{ user.age }}</li>-->
+      <li v-for="(value, name, index) in product" :key="value">{{ index }} {{ name }} {{ value }}</li>
+
+      <strong>{{ allUsersTotalAge }}</strong>
       <div>
-       <!-- <input type="text" @keyup.enter="addNewColor"> -->
+        <!-- <input type="text" @keyup.enter="addNewColor"> -->
         <input type="text" @keyup.enter="deleteProp">
       </div>
     </ul>
@@ -21,13 +23,26 @@ export default {
   // components: {HelloWorld}
   data: () => ({
     colors: ['red', 'blue', 'black'],
-    users: [{name: 'maksim', age: 30, id: '1'}, {name: 'ivan', age: 30, id: '2'}],
+    users: [
+      {name: 'maksim', age: 28, id: '1'},
+      {name: 'ivan', age: 34, id: '2'}
+    ],
     product: {
       brand: 'Apple',
       model: 'Iphone 11',
       price: '$1000'
     }
   }),
+  computed: {
+    allUsersTotalAge() {
+      let total = 0;
+      for (let i = 0; i < this.users; i++) {
+        let age = this.users[i].age;
+        total += age;
+      }
+      return total;
+    }
+  },
   methods: {
     addNewColor(e) {
       this.colors.push(e.target.value);
