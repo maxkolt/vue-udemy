1b48c7bf40b6e7b8da30f3a87b36ad4974070ca0
Администратор <12345kolt@gmail.com>
2021-03-12 11:49:24 +0300

    Vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="привет мир"/>
-  </div>
+  <section id="app">
+    <h1>{{ firstName }}</h1>
+    <h2>{{ num * 3 }}</h2>
+    <h3>{{ obj }}</h3>
+    <a v-bind:href="linkUrl">Link</a>
+
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  // components: {HelloWorld}
+  data: () => ({
+    firstName: 'Max',
+    num: 10,
+    obj: {
+      age: 30
+    },
+    linkUrl: 'http//google.com'
+  }),
+  methods:{
   }
+
 }
+
 </script>
 
 <style>
